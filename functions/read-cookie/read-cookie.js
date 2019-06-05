69526505ce70b7723a057a4571cc677090e73fce
Divya Sasidharan <hello@shortdiv.com>
2019-06-05 15:30:08 -0400

    fix: read cookie set header

--- functions/read-cookie/read-cookie.js
+++ functions/read-cookie/read-cookie.js
@@ -1,8 +1,25 @@
+const cookie = require("cookie");
+const jsonwebtoken = require("jsonwebtoken");
+
 exports.handler = function(event, context, callback) {
   const { headers } = event;
+  const token = headers.authorization;
+
+  const netlifyToken = jsonwebtoken.decode(token);
+
+  const netlifyCookie = cookie.serialize("nf_jwt", token, {
+    secure: true,
+    path: "/",
+    expires: new Date(netlifyToken.exp)
+  });
 
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ headers })
+    headers: {
+      "Set-Cookie": netlifyCookie,
+      "Cache-Control": "no-cache",
+      "Content-Type": "text/html"
+    },
+    body: JSON.stringify({ auth: netlifyToken })
   });
 };
