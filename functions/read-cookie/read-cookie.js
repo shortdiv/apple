321d4268fd1626dff58eecacae39af5d253c9151
Divya Sasidharan <hello@shortdiv.com>
2019-06-05 15:36:56 -0400

    testing testing

--- functions/read-cookie/read-cookie.js
+++ functions/read-cookie/read-cookie.js
@@ -1,25 +1,17 @@
-const cookie = require("cookie");
-const jsonwebtoken = require("jsonwebtoken");
-
 exports.handler = function(event, context, callback) {
   const { headers } = event;
   const token = headers.authorization;
 
-  const netlifyToken = jsonwebtoken.decode(token);
+  // const netlifyToken = jsonwebtoken.decode(token)
 
-  const netlifyCookie = cookie.serialize("nf_jwt", token, {
-    secure: true,
-    path: "/",
-    expires: new Date(netlifyToken.exp)
-  });
+  // const netlifyCookie = cookie.serialize("nf_jwt", token, {
+  //   secure: true,
+  //   path: "/",
+  //   expires: new Date(netlifyToken.exp)
+  // });
 
   callback(null, {
     statusCode: 200,
-    headers: {
-      "Set-Cookie": netlifyCookie,
-      "Cache-Control": "no-cache",
-      "Content-Type": "text/html"
-    },
-    body: JSON.stringify({ auth: netlifyToken })
+    body: JSON.stringify({ token })
   });
 };
