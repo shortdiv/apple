a3522a4a676be03c6ee7fc2155d58ab36f4264e1
Divya Sasidharan <hello@shortdiv.com>
2019-06-05 16:26:20 -0400

    checking headers

--- functions/read-cookie/read-cookie.js
+++ functions/read-cookie/read-cookie.js
@@ -1,43 +1,37 @@
 const cookie = require("cookie");
-const jsonwebtoken = require("jsonwebtoken");
 
 exports.handler = function(event, context, callback) {
   const { headers } = event;
-  const bearerToken = headers.authorization;
-  let netlifyToken = bearerToken.split("Bearer ")[1];
+  const cookieHeader = headers.cookie || "";
+  const cookies = cookie.parse(cookieHeader);
 
-  netlifyToken = jsonwebtoken.decode(netlifyToken);
+  // netlifyToken = jsonwebtoken.decode(netlifyToken);
 
-  const netlifyCookie = cookie.serialize("nf_jwt", netlifyToken, {
-    secure: true,
-    path: "/",
-    expires: new Date(netlifyToken.exp)
-  });
+  // const netlifyCookie = cookie.serialize("nf_jwt", netlifyToken, {
+  //   secure: true,
+  //   path: "/",
+  //   expires: new Date(netlifyToken.exp)
+  // });
 
-  const html = `
-  <html lang="en">
-    <head>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <noscript>
-        <meta http-equiv="refresh" content="0; url="https://apple-security.netlify.com" />
-      </noscript>
-    </body>
-    <script>
-      setTimeout(function(){
-        window.location.href = "https://apple-security.netlify.com"
-      }, 0)
-    </script>
-  </html>`;
+  // const html = `
+  // <html lang="en">
+  //   <head>
+  //     <meta charset="utf-8">
+  //   </head>
+  //   <body>
+  //     <noscript>
+  //       <meta http-equiv="refresh" content="0; url="https://apple-security.netlify.com" />
+  //     </noscript>
+  //   </body>
+  //   <script>
+  //     setTimeout(function(){
+  //       window.location.href = "https://apple-security.netlify.com"
+  //     }, 0)
+  //   </script>
+  // </html>`;
 
   callback(null, {
     statusCode: 200,
-    headers: {
-      "Set-Cookie": netlifyCookie,
-      "Cache-Control": "no-cache",
-      "Content-Type": "text/html"
-    },
-    body: html
+    body: JSON.stringify({ cookieHeader, cookies })
   });
 };
