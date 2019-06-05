aef9fea3251147a8409d3aac420cae92897a6f0e
Divya Sasidharan <hello@shortdiv.com>
2019-06-05 16:51:56 -0400

    check cookie is sent

--- functions/read-cookie/read-cookie.js
+++ functions/read-cookie/read-cookie.js
@@ -1,36 +1,39 @@
-const jsonwebtoken = require("jsonwebtoken");
-
 exports.handler = function(event, context, callback) {
   const params = event.queryStringParameters;
   const cookie = params.cookie;
 
-  const netlifyToken = jsonwebtoken.decode(cookie);
+  // const netlifyToken = jsonwebtoken.decode(cookie);
 
-  const netlifyCookie = cookie.serialize("nf_jwt", netlifyToken, {
-    secure: true,
-    path: "/",
-    expires: new Date(netlifyToken.exp)
-  });
+  // const netlifyCookie = cookie.serialize("nf_jwt", netlifyToken, {
+  //   secure: true,
+  //   path: "/",
+  //   expires: new Date(netlifyToken.exp)
+  // });
 
-  // const html = `
-  // <html lang="en">
-  //   <head>
-  //     <meta charset="utf-8">
-  //   </head>
-  //   <body>
-  //     <noscript>
-  //       <meta http-equiv="refresh" content="0; url="https://apple-security.netlify.com" />
-  //     </noscript>
-  //   </body>
-  //   <script>
-  //     setTimeout(function(){
-  //       window.location.href = "https://apple-security.netlify.com"
-  //     }, 0)
-  //   </script>
-  // </html>`;
+  const html = `
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <noscript>
+        <meta http-equiv="refresh" content="0; url="https://apple-security.netlify.com" />
+      </noscript>
+    </body>
+    <script>
+      setTimeout(function(){
+        window.location.href = "https://apple-security.netlify.com"
+      }, 0)
+    </script>
+  </html>`;
 
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ cookie, netlifyCookie })
+    headers: {
+      "Set-Cookie": cookie,
+      "Cache-Control": "no-cache",
+      "Content-Type": "text/html"
+    },
+    body: html
   });
 };
