6f48f4447bba12b358e5d40002e8d3fbb6e5a0cd
Divya Sasidharan <hello@shortdiv.com>
2019-06-05 16:29:24 -0400

    check headers

--- functions/read-cookie/read-cookie.js
+++ functions/read-cookie/read-cookie.js
@@ -32,6 +32,6 @@ exports.handler = function(event, context, callback) {
 
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ cookieHeader, cookies })
+    body: JSON.stringify({ cookieHeader, cookies, headers })
   });
 };
